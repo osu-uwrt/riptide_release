df8a49db8b1d5872446b3e7cfd68f18684842cd1
Mason Erwine <merwine33@gmail.com>
2025-03-24 19:31:25 +0000

    Fix Zenoh Config & Zed SDK Install

---
+++ scripts/install-zed-sdk.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+wget -O ZED_SDK.run https://download.stereolabs.com/zedsdk/5.0/l4t36.4/jetsons
+chmod +x ZED_SDK.run
+./ZED_SDK.run -- silent
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -3,5 +3,6 @@
 bash configure-jetpack.bash
 bash install-ros.bash
 bash install-zenoh.bash
+bash install-zed-sdk.bash
 
 sudo reboot
