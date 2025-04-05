8db85d139900145e07b05b73d5e1cfcac0465c89
Mason Erwine <mason@erwine.dev>
2025-04-05 09:37:21 +0000

    update setup.bash

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-bash configure-jetpack.bash
-bash install-ros.bash
-bash install-zed-sdk.bash
+bash subscripts/configure-jetpack.bash
+bash subscripts/install-ros.bash
+bash subscripts/configure-fastdds.bash
+bash subscripts/install-zed-sdk.bash
 
 sudo reboot
