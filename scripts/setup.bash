541ae9bb1a62c31486761c4959237ffb910bf0d8
Mason Erwine <mason@erwine.dev>
2025-04-05 09:39:53 +0000

    Update permissions

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash subscripts/configure-jetpack.bash
+sudo bash subscripts/configure-jetpack.bash
 bash subscripts/install-ros.bash
 bash subscripts/configure-fastdds.bash
 bash subscripts/install-zed-sdk.bash
--- scripts/subscripts/configure-jetpack.bash
+++ scripts/subscripts/configure-jetpack.bash
@@ -2,11 +2,11 @@
 
 # Super Speedy Fan
 echo "Updating fan control"
-sudo systemctl stop nvfancontrol
+systemctl stop nvfancontrol
 mv /etc/nvfancontrol.conf /etc/nvfancontrol.conf.bkup
 cp configs/nvfancontrol.conf /etc/nvfancontrol.conf
-sudo rm /var/lib/nvfancontrol/status
-sudo systemctl start nvfancontrol
+rm /var/lib/nvfancontrol/status
+systemctl start nvfancontrol
 
 # Configure Network with Systemd Network
 echo "Configuring Network"
@@ -18,12 +18,12 @@ cp configs/systemd/*.link /etc/systemd/network
 cp configs/systemd/*.network /etc/systemd/network
 
 # Switch from NetworkManager to systemd-networkd on next reboot
-sudo systemctl disable NetworkManager
-sudo systemctl mask NetworkManager
-sudo systemctl unmask systemd-networkd
-sudo systemctl enable systemd-networkd
+systemctl disable NetworkManager
+systemctl mask NetworkManager
+systemctl unmask systemd-networkd
+systemctl enable systemd-networkd
 
 # add the dvl to the hosts file
-sudo sh -c "echo "192.168.1.212    uwrt-dvl" >> /tmp/hosts"
+echo "192.168.1.212    uwrt-dvl" >> /tmp/hosts
 
-sudo cp configs/systemd/mnt-sd.mount /etc/systemd/system
+cp configs/systemd/mnt-sd.mount /etc/systemd/system
