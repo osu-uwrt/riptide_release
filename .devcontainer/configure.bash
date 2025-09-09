41424be6344ee69ac67a4858db0bae227131b893
Mason Erwine <mason@erwine.dev>
2025-09-08 20:53:47 -0400

    Remove problamatic things

--- .devcontainer/configure.bash
+++ .devcontainer/configure.bash
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+echo Configuring Workspace
+
 cd /wokspaces/riptide_release
 
 if [[ ! -e ~/.configured ]]; then
+
+
+
     # Add this file to bashc
     echo "source /workspaces/riptide_release/.devcontainer/setup.bash" >> ~/.bashrc
+
 
     # Init & Pull Submodules
     git submodule init
     git pull --recurse-submodules
 
     # Update Packages
-    apt update && apt upgrade -y
+    sudo apt update && sudo apt upgrade -y
+
+    # Install Clang
+    apt install -y lsb-release wget software-properties-common gnupg
+    bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
     # Apiltag ROS breaks rosdep
     rm -r /workspaces/riptide_release/src/apriltag_ros
 
     # Install Deps
-    rosdep install -iry --from-paths /workspaces/riptide_release/src
+    sudo rosdep install -iry --from-paths /workspaces/riptide_release/src
 
     touch ~/.configured
 fi
-
-exit 0
