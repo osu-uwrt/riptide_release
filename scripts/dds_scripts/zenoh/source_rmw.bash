323f210063c6e121c4001c3b9081b486d768f73b
osu-uwrt-bot2 <osu.uwrt@gmail.com>
2026-03-23 16:32:29 -0400

    Stop ROS daemon on Zenoh source

--- scripts/dds_scripts/zenoh/source_rmw.bash
+++ scripts/dds_scripts/zenoh/source_rmw.bash
@@ -2,3 +2,5 @@ export ZENOH_ROUTER_CONFIG_URI=$HOME/osu-uwrt/release/scripts/dds_scripts/zenoh/
 export ZENOH_SESSION_CONFIG_URI=$HOME/osu-uwrt/release/scripts/dds_scripts/zenoh/zenoh_session.json5
 
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+
+ros2 daemon stop
